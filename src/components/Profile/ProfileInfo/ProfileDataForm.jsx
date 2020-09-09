274fda1a196cfecc2f05db97408704dda2338af0
VladSWEED <57415244+VladSWEED@users.noreply.github.com>
2020-09-10 01:01:07 +0300

    update PROFILE day 1

--- src/components/Profile/ProfileInfo/ProfileDataForm.jsx
+++ src/components/Profile/ProfileInfo/ProfileDataForm.jsx
@@ -2,10 +2,28 @@ import React from 'react';
 
 
 const ProfileDataForm = ({ profile }) => {
-    return <div>
-    FORM
+    return <form>
+    <div><button onClick={goToEditMode}>save</button></div>
+    <div>
+        <b>Full name</b>:{profile.fullName}
     </div>
-
+    <div>
+        <b>Looking for a job</b>:{profile.lookingForAJob ? "yes" : "no"}
+    </div>
+    {profile.lookingForAJob &&
+        <div>
+            <b>My professional skills</b>:{profile.lookingForAJobDescription}
+        </div>
+    }
+    <div>
+        <b>About me</b>:{profile.aboutMe}
+    </div>
+    <div>
+        <b>Contacts</b>:{Object.keys(profile.contacts).map(key => {
+            return <Contact key={key} contactTitle={key} contactValue={profile.contacts[key]} />
+        })}
+    </div>
+</form>
 }
 
 export default ProfileDataForm;
