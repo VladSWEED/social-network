97f33c62979c98c407c5552e14eec27da0db99cf
VladSWEED <57415244+VladSWEED@users.noreply.github.com>
2020-09-13 23:57:08 +0300

    98-captcha END

--- src/redux/auth-reducer.js
+++ src/redux/auth-reducer.js
@@ -15,11 +15,11 @@ let initialState = {
 export const authReducer = (state = initialState, action) => {
     switch (action.type) {
         case SET_USER_DATA:
-        case GET_CAPTCHA_URL_SUCCESS:{
+        case GET_CAPTCHA_URL_SUCCESS:
             return {
             ...state,
             ...action.payload
-        }}
+        }
         default:
             return state;
     }
